1315fd42eaf033d67e567e13eb974435e4ff3833
willsampson <wsampson@uchicago.edu>
2023-01-27 17:46:28 +0000

    omni 2

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -11,5 +11,40 @@ class MathController < ActionController::Base
     render( {:template => "math_templates/subtract_results.html.erb"})
   end
 
+  def plus_form
+    render( {:template => "math_templates/add_form.html.erb"})
+  end
+
+  def plus_results
+    @first = params.fetch("beak").to_f
+    @second = params.fetch("wing").to_f
+    @result = @second + @first
+
+    render( {:template => "math_templates/add_results.html.erb"})
+  end
+
+  def times_form
+    render( {:template => "math_templates/multiply_form.html.erb"})
+  end
+
+  def times_results
+    @first = params.fetch("cake").to_f
+    @second = params.fetch("ice_cream").to_f
+    @result = @second * @first
+
+    render( {:template => "math_templates/multiply_results.html.erb"})
+  end
+
+  def divided_form
+    render( {:template => "math_templates/divide_form.html.erb"})
+  end
+
+  def divided_results
+    @first = params.fetch("blond").to_f
+    @second = params.fetch("black").to_f
+    @result = @first / @second
+
+    render( {:template => "math_templates/divide_results.html.erb"})
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,16 @@ Rails.application.routes.draw do
 
 get("/subtract", { :controller => "math", :action => "minus_form"})
 get("/wizard_subtract", { :controller => "math", :action => "minus_results"})
+
+get("/add", { :controller => "math", :action => "plus_form"})
+get("/wizard_add", { :controller => "math", :action => "plus_results"})
+
+get("/multiply", { :controller => "math", :action => "times_form"})
+get("/wizard_multiply", { :controller => "math", :action => "times_results"})
+
+get("/divide", { :controller => "math", :action => "divided_form"})
+get("/wizard_divide", { :controller => "math", :action => "divided_results"})
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
